65167b8a266f95a74666ce007c7aa35f292de615
Jakub Zagórski <jaqobbdev@gmail.com>
2020-08-05 19:13:32 +0200

    Allow server owners to disable attaching special hover and click events on >= 1.15 servers

--- src/main/java/dev/jaqobb/messageeditor/MessageEditorPlugin.java
+++ src/main/java/dev/jaqobb/messageeditor/MessageEditorPlugin.java
@@ -73,10 +73,10 @@ public final class MessageEditorPlugin extends JavaPlugin {
 		this.getLogger().log(Level.INFO, "Checking if copying to clipboard is supported on your server...");
 		try {
 			ClickEvent.Action.valueOf("COPY_TO_CLIPBOARD");
-			this.getLogger().log(Level.INFO, "Copying to clipboard is supported on your server, enabling attaching special hover and click events.");
-			this.attachSpecialHoverAndClickEvents = true;
+			this.getLogger().log(Level.INFO, "Copying to clipboard is supported on your server.");
+			this.attachSpecialHoverAndClickEvents = this.getConfig().getBoolean("attach-special-hover-and-click-events", true);
 		} catch (IllegalArgumentException exception) {
-			this.getLogger().log(Level.INFO, "Copying to clipboard is not supported on your server, disabling attaching special hover and click events...");
+			this.getLogger().log(Level.INFO, "Copying to clipboard is not supported on your server.");
 			this.getLogger().log(Level.INFO, "Attaching special hover and click events works only on server version at least 1.15.");
 			this.attachSpecialHoverAndClickEvents = false;
 		}
