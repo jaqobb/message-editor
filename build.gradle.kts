11bbb7fd8f78fdb096ca462bfeb6d30243f468a7
Jakub Zagórski <jaqobbdev@gmail.com>
2020-12-30 14:34:37 +0100

    2.0.4-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "dev.jaqobb"
-version = "2.0.3"
+version = "2.0.4-SNAPSHOT"
 description = "Spigot plugin that allows editing in-game messages that were previously unmodifiable"
 
 java {
