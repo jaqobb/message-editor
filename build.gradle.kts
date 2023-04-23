d4495b1dd4eaa8e77ba4bd94d36bb796ee59c0b7
Jakub Zagórski <jaqobbdev@gmail.com>
2023-04-23 18:44:12 +0200

    Bump Gradle plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
-    id("net.minecrell.plugin-yml.bukkit") version "0.5.2"
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("net.minecrell.plugin-yml.bukkit") version "0.5.3"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "dev.jaqobb"
